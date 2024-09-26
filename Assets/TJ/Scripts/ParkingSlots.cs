725d670c4bff598cdaa5373204fad553590fc224
NhanLe1999 <55460318+NhanLe1999@users.noreply.github.com>
2024-09-26 17:17:56 +0700

    first-commit

---
+++ Assets/TJ/Scripts/ParkingSlots.cs
@@ -0,0 +1,43 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+
+namespace TJ.Scripts
+{
+    public class ParkingSlots : MonoBehaviour
+    {
+        public Transform enterPoint;
+        public Transform stopPoint;
+
+        public bool isOccupied;
+        [SerializeField] private GameObject normal;
+        [SerializeField] private GameObject locked;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            enterPoint = transform.GetChild(0).transform;
+            stopPoint = transform.GetChild(1).transform;
+        }
+
+        /*private void OnTriggerStay(Collider other)
+        {
+            isOccupied = true;
+        }*/
+        private void OnMouseDown()
+        {
+            if (GameManager.instance.gameOver || EventSystem.current.IsPointerOverGameObject())
+                return;
+            var slots = ParkingManager.instance.slots;
+            if (!slots.Contains(this))
+            {
+                slots.Add(this);
+                locked.SetActive(false);
+                normal.SetActive(true);
+            }
+            Debug.Log("Added Slot");
+        }
+    }
+}
